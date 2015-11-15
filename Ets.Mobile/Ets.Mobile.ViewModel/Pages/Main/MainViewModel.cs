6890b53bd66787bbf5b6b59553b8432f3f8a37cd
Martin Gemme <martin.gemme@outlook.com>
2015-11-15 00:06:34 -0500

    Removed some usings

--- Ets.Mobile/Ets.Mobile.ViewModel/Pages/Main/MainViewModel.cs
+++ Ets.Mobile/Ets.Mobile.ViewModel/Pages/Main/MainViewModel.cs
@@ -1,21 +1,8 @@
 ﻿using System;
-using System.Reactive;
 using System.Runtime.Serialization;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
-using Ets.Mobile.Entities.Signets;
 using Ets.Mobile.ViewModel.Bases;
-using Ets.Mobile.ViewModel.Contracts;
 using Ets.Mobile.ViewModel.Contracts.Main;
-using Ets.Mobile.ViewModel.Contracts.Shared;
-using Ets.Mobile.ViewModel.Pages.Grade;
-using Ets.Mobile.ViewModel.Pages.Schedule;
 using ReactiveUI;
-using Splat;
-using System.Reactive.Linq;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Reactive.Subjects;
 
 namespace Ets.Mobile.ViewModel.Pages.Main
 {
