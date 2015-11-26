9d1e14483522233172ef43fd33b01651a66c4864
Martin Gemme <martin.gemme@outlook.com>
2015-11-26 04:15:26 -0500

    Added some untracked files

---
+++ Ets.Mobile/Ets.Mobile.ViewModel/Messaging/EmptyMessageContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Messaging.Interfaces.Common;
+
+namespace Ets.Mobile.ViewModel.Messaging
+{
+    public class EmptyMessageContent : Exception, IMessagingContent
+    {
+        public EmptyMessageContent(string title, string message) : base(message)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+    }
+}
