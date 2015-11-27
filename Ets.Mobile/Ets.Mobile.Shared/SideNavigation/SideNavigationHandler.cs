943b22541eadac084bad37e69fa104b0d94d8a4d
Martin Gemme <martin.gemme@outlook.com>
2015-11-27 02:06:46 -0500

    SideNavigationHandler

---
+++ Ets.Mobile/Ets.Mobile.Shared/SideNavigation/SideNavigationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Ets.Mobile.ViewModel.Bases;
+
+namespace Ets.Mobile.SideNavigation
+{
+    public class SideNavigationHandler
+    {
+        private static int count = 0;
+        public static Action<AppBar, ViewModelBase> HandleForAppBar = (appBar, vm) =>
+        {
+            if (appBar != null && count == 0)
+            {
+                appBar.Visibility = Visibility.Visible;
+                vm.SideNavigation.IsSideNavigationVisibleSubject.Subscribe(
+                    isVisible =>
+                    {
+                        appBar.Visibility = isVisible ? Visibility.Collapsed : Visibility.Visible;
+                    });
+                count++;
+            }
+        };
+    }
+}
