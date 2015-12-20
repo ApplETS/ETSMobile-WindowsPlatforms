ef3f158e91cadf87d79c81b7a3ff5234e8e79684
Martin Gemme <martin.gemme@outlook.com>
2015-12-20 11:28:54 -0500

    Converter to Debug the binding

---
+++ Ets.Mobile/Modules/Xaml.Converters/DebugProxyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Xaml.Converters
+{
+    public class DebugProxyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+#if DEBUG
+            return value;
+#endif
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+#if DEBUG
+            return value;
+#endif
+        }
+    }
+}
