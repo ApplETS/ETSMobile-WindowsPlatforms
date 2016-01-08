afcd535d6bf0e35d0b1553efaf8afe5e4b352d75
Martin Gemme <martin.gemme@outlook.com>
2016-01-07 19:43:22 -0500

    Business Services uses Tasks

--- Ets.Mobile/Ets.Mobile.Business/Contracts/IMoodleBusinessService.cs
+++ Ets.Mobile/Ets.Mobile.Business/Contracts/IMoodleBusinessService.cs
@@ -3,22 +3,22 @@ using Ets.Mobile.Business.Entities.Moodle.CoursesContent;
 using Ets.Mobile.Business.Entities.Moodle.SiteInfo;
 using Ets.Mobile.Business.Entities.Moodle.Token;
 using Refit;
-using System;
+using System.Threading.Tasks;
 
 namespace Ets.Mobile.Business.Contracts
 {
     public interface IMoodleBusinessService
     {
         [Post("/login/token.php?username={username}&password={password}&service=moodle_mobile_app")]
-        IObservable<MoodleToken> Token(string username, string password);
+        Task<MoodleToken> Token(string username, string password);
 
         [Post("/webservice/rest/server.php?moodlewsrestformat=json&wstoken={token}&wsfunction=moodle_webservice_get_siteinfo")]
-        IObservable<MoodleSiteInfo> SiteInfo(string token);
+        Task<MoodleSiteInfo> SiteInfo(string token);
 
         [Post("/webservice/rest/server.php?moodlewsrestformat=json&wstoken={token}&wsfunction=moodle_enrol_get_users_courses&userid={userId}")]
-        IObservable<MoodleCourse[]> Courses(string token, int userId);
+        Task<MoodleCourse[]> Courses(string token, int userId);
 
         [Post("/webservice/rest/server.php?moodlewsrestformat=json&wstoken={token}&wsfunction=core_course_get_contents&courseid={courseId}")]
-        IObservable<MoodleCourseContent[]> CoursesContents(string token, int courseId);
+        Task<MoodleCourseContent[]> CoursesContents(string token, int courseId);
     }
 }
