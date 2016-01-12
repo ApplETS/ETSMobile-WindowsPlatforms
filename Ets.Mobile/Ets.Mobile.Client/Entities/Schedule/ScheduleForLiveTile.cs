2212ce108a1c1cf6ef68abdc6e55bc62ae2d8c26
Martin Gemme <martin.gemme@outlook.com>
2016-01-12 17:35:02 -0500

    Add Services to have access to the schedule of the currently logged-in user

---
+++ Ets.Mobile/Ets.Mobile.Client/Entities/Schedule/ScheduleForLiveTile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ets.Mobile.Client.Entities.Schedule
+{
+    public class ScheduleForLiveTile
+    {
+        public ScheduleForLiveTile(string activityName, string location, string name, string group, DateTime startTime, DateTime endTime)
+        {
+            ActivityName = activityName;
+            Location = location;
+            Name = name;
+            Group = group;
+            StartDate = startTime;
+            EndDate = endTime;
+        }
+
+        public string ActivityName { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Ets.Mobile/Ets.Mobile.Client/Services/LiveTileAndLockScreenService.cs
@@ -0,0 +1,39 @@
+﻿using Akavache;
+using Ets.Mobile.Client.Entities.Schedule;
+using Ets.Mobile.Entities.Signets;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace Ets.Mobile.Client.Services
+{
+    public class LiveTileAndLockScreenService
+    {
+        public static async Task<ScheduleForLiveTile> GetCurrentOrIncomingCourse()
+        {
+            BlobCache.ApplicationName = "EtsMobile";
+            var cache = BlobCache.UserAccount;
+            var now = new DateTime(2016, 01, 11, 16, 30, 00);
+            return
+                await cache.GetObject<SemesterVm[]>("semesters")
+                    .Where(x => x.FirstOrDefault(y => y.StartDate <= now && y.EndDate > now) != null)
+                    .Select(semesters => semesters.FirstOrDefault(y => y.StartDate <= now && y.EndDate > now))
+                    .SelectMany(currentSemester => cache.GetObject<ScheduleVm[]>("schedule_" + currentSemester.AbridgedName))
+                    // Get Today's Courses
+                    .Select(courses => courses.Where(c => c.StartDate.Date == now.Date).OrderBy(c => c.StartDate))
+                    // Get Current Course or Following Course
+                    .FirstOrDefaultAsync(courses => courses.Any(c => (c.StartDate <= now && c.EndDate > now) || c.StartDate > now))
+                    .SelectMany(courses =>
+                    {
+                        var currentOrIncomingCourse =
+                            courses?.FirstOrDefault(c => (c.StartDate <= now && c.EndDate > now) || c.StartDate > now);
+                        return currentOrIncomingCourse != null ? Observable.Return(currentOrIncomingCourse) : Observable.Return(default(ScheduleVm));
+                    })
+                    // Convert to simple object
+                    .Select(scheduleItem => scheduleItem != null ? new ScheduleForLiveTile(scheduleItem.ActivityName, scheduleItem.Location, scheduleItem.Name, scheduleItem.Group, scheduleItem.StartDate, scheduleItem.EndDate) : default(ScheduleForLiveTile))
+                    .ToTask();
+        }
+    }
+}
