9a40750d399c5a7c0a75355cc67c17901e134e78
Martin Gemme <martin.gemme@outlook.com>
2016-01-12 17:24:22 -0500

    Modified Agent Configuration to be Windows Runtime

--- Ets.Mobile/Ets.Mobile.Agent.WinRT/Properties/AssemblyInfo.cs
+++ Ets.Mobile/Ets.Mobile.Agent.WinRT/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,7 +11,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ets.Mobile.Agent.WinRT")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
