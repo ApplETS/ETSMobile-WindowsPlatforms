389f91cde4569f64a3a8ead1278d784ef402d2b0
Martin Gemme <martin.gemme@outlook.com>
2016-01-12 22:14:22 -0500

    Add Views, localizable text and Rx-Events to the project to manage the background task

---
+++ Ets.Mobile/Ets.Mobile.WindowsPhone/Content/Settings/Options.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Ets.Mobile.ViewModel.Contracts.Settings;
+using System;
+using System.Reactive.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using ReactiveUI;
+using System.Reactive.Concurrency;
+
+namespace Ets.Mobile.Content.Settings
+{
+    public sealed partial class Options : UserControl
+    {
+        public Options()
+        {
+            InitializeComponent();
+            this.Events().DataContextChanged.Subscribe(args =>
+            {
+                var options = args.NewValue as IOptionsViewModel;
+                if (options != null)
+                {
+                    options.HandleScheduleBackgroundService.IsExecuting.Subscribe(isExecuting =>
+                    {
+                        ShowSchedule.IsEnabled = !isExecuting;
+                        LoadingShowScheduleChanged.Visibility = isExecuting ? Visibility.Visible : Visibility.Collapsed;
+                    });
+                }
+            });
+        }
+    }
+}
