33e3ecc53b8452da114dbfdba40672b3db44ba19
Martin Gemme <martin.gemme@outlook.com>
2016-01-19 01:23:32 -0500

    Move Themes to Interfaces and String Hexes to not depend upon Windows Runtime (again, removes the hassle to deal with Windows Runtime and only Test the Services)

---
+++ Ets.Mobile/Modules/Themes/Extensions/ColorAsStringExtensions.cs
@@ -0,0 +1,29 @@
+﻿//#if WINDOWS_PHONE_APP || WINDOWS_UWP
+//using System.Globalization;
+//using System.Linq;
+//using System.Runtime.InteropServices.WindowsRuntime;
+//using Themes.Entities;
+//using Windows.UI;
+
+//namespace Themes.Extensions
+//{
+//    public static class ColorAsStringExtensions
+//    {
+//        public static Color ToColor(this ColorAsString color)
+//        {
+//            return new Color
+//            {
+//                A = byte.Parse(color.HexColor.Substring(7, 2), NumberStyles.AllowHexSpecifier),
+//                R = byte.Parse(color.HexColor.Substring(1, 2), NumberStyles.AllowHexSpecifier),
+//                G = byte.Parse(color.HexColor.Substring(3, 2), NumberStyles.AllowHexSpecifier),
+//                B = byte.Parse(color.HexColor.Substring(5, 2), NumberStyles.AllowHexSpecifier)
+//            };
+//        }
+
+//        public static Color[] ToColorArray([ReadOnlyArray] this ColorAsString[] colors)
+//        {
+//            return colors.Select(c => c.ToColor()).ToArray();
+//        }
+//    }
+//}
+//#endif
